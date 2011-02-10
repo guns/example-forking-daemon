35c503dd6b95e72e76a1413c8246122fb3267bc2
guns <sung@metablu.com>
2011-02-10 14:14:47 -0600

    reorder functions

--- forking-daemon.c
+++ forking-daemon.c
@@ -61,212 +61,51 @@ int     trap_signals(bool on);
 void    restart_children();
 void    terminate_children();
 
-/* When passed true, traps signals and assigns handlers as defined in sigpairs[]
- * When passed false, resets all trapped signals back to their default behavior.
- */
-int trap_signals(bool on)
-{
-    int i;
-    struct sigaction dfl;       /* the handler object */
-
-    dfl.sa_handler = SIG_DFL;   /* for resetting to default behavior */
-
-    /* Loop through all registered signals and either set to the new handler
-     * or reset them back to the default */
-    for (i = 0; i < sigcount; ++i) {
-        /* notice that the second parameter takes the address of the handler */
-        if (sigaction(sigpairs[i].signal, on ? &sigpairs[i].action : &dfl, NULL) < 0)
-            return false;
-    }
-
-    return true;
-}
-
-/* Forks a child process, and returns true if successful.
- * The forked child blocks and randomly dies.
+/* Entry routine; parse command line options and launch master process.
  *
- * If this program did any work, the main communication between parent and child
- * could take place through a pipe(2).
+ * REGARDING fork():
+ *
+ * Fork is a strange and unique system call. Along with exec(), it forms one of
+ * the core features of Unix.
+ *
+ * At the moment fork() is called,
+ *   - if the fork failed, it returns -1
+ *   - if the fork succeeds, a duplicate of the parent process is formed,
+ *     except that the caller is designated as the parent of new process
+ *   - fork() returns the pid of the newly created process
+ *   - BUT IN THE CHILD process, fork returns 0
+ *
+ * Because the child process will see fork() returning 0, and the parent will
+ * see fork() returning a pid, we can carefully diverge the behavior of both to
+ * do what we want!
  */
-bool child(int id)
+int main(int argc, char *argv[])
 {
     pid_t   pid;
+    int     status;
 
-    /* see main() for discussion on fork() */
-    pid = fork();
+    optparse(argc, argv);
 
-    if (pid < 0) {
-        return false;
-    } else if (pid > 0) {
-        printf("Spawning child %d (pid %d)\n", id, pid);
-        children[id] = pid; /* record the child pid */
-        return true;
-    }
-
-    /* Child process continues here */
-
-    /* Children processes are exact (almost) copies of the parent!
-     * Including signal traps! Our parent has a SIGTERM handler, and this child
-     * will dutifully execute the handler on SIGTERM unless we reset all
-     * signals back to their default handlers */
-    if (!trap_signals(false)) {
-        fprintf(stderr, "Child %d: trap_signals() failed!\n", id);
-        exit(1);
-    }
-
-    /* Block, and randomly die.
-     * If you're on Linux, arc4random() is why you need to link to libbsd
-     * (because it works, and I'm lazy) */
-    while (1) {
-        arc4random_stir();
-        if (arc4random() % 20)
-            sleep(1);
-        else
-            break;
-    }
-
-    exit(0);
-}
-
-/* Master's SIGCHLD handler.
- *
- * When a process is fork()ed by a process, the new process is an exact copy
- * of the old process, except for a few values, one of which is that the parent
- * pid of the child is that of the process that forked it.
- *
- * When this child exits, the signal SIGCHLD is sent to the parent process to
- * alert it. By default, the signal is ignored, but we can take this opportunity
- * to restart any children that have died.
- *
- * There are many ways to determine which children have died, but the most
- * portable method is to use the wait() family of system calls.
- *
- * A dead child process releases its memory, but sticks around so that any
- * interested parties can determine how they died (exit status). Calling wait()
- * in the master collects the status of the first available dead process, and
- * removes it from the process table.
- *
- * If wait() is never called by the parent, the dead child sticks around as a
- * "zombie" process, marked with status `Z' in ps output. If the parent process
- * exits without ever calling wait, the zombie process does not disappear, but
- * is inherited by the root process (its parent pid is set to 1).
- *
- * Because SIGCHLD is an asynchronous signal, it is possible that if many
- * children die simultaneously, the parent may only notice one SIGCHLD when many
- * have been sent. In order to beat this edge case, we can simply loop through
- * all the known children and call waitpid() in non-blocking mode to see if they
- * have died, and spawn a new one in their place.
- */
-void restart_children()
-{
-    int     i, status[0xff];    /* array of exit statuses, just in case */
-    pid_t   pid;
-
-    /* Racing signals may let dead children through the cracks,
-     * so check all children
-     */
-    for (i = 0; i < options.jobs; ++i) {
-        pid = waitpid(children[i], &status[i], WNOHANG); /* non-blocking! */
+    if (options.daemonize) {
+        pid = fork();
 
         if (pid < 0) {
-            perror("waitpid()");
-        } else if (!pid) {
-            /* waitpid returns 0 when the process is alive and well */
-            continue;
-        } else {
-            printf("Master: reaped dead child %d (pid %d)\n", i, pid);
-            child(i); /* relaunch this particular child */
+            /* fork failed, probably due to resource limits */
+            perror("fork()");
+            return errno;
+        } else if (pid > 0) {
+            /* fork successful! Tell us the forked processes's pid and exit */
+            printf("Forked master process: %d\n", pid);
+            return 0;
         }
-    }
-}
 
-/* Master's kill switch
- *
- * It's important to ensure that all children have exited before the master
- * exits so no root zombies are created. The default handler for SIGINT sends
- * SIGINT to all children, but this is not true with SIGTERM.
- */
-void terminate_children()
-{
-    int     i, status;
-    pid_t   pid;
+        /* Forked process continues here */
 
-    printf("Termination signal received! Killing children");
-
-    /* Reset all signals! If the SIGCHLD handler in particular is not reset,
-     * all terminated children will be restarted
-     */
-    trap_signals(false);
-
-    for (i = 0; i < options.jobs; ++i)
-        kill(children[i], SIGTERM);
-
-    /* reap all children as they die, until none are left;
-     * at that point wait() returns -1 */
-    while ((pid = wait(&status)) != -1)
-        printf(".");
-
-    printf("\nAll children reaped, shutting down.\n");
-    exit(0);
-}
-
-/* Populate our array of custom handler objects */
-void register_signals()
-{
-    int i = 0;
-
-    sigpairs[i].signal            = SIGCHLD;
-    sigpairs[i].action.sa_handler = &restart_children;
-    /* Don't send SIGCHLD when a process has been frozen (e.g. Ctrl-Z) */
-    sigpairs[i].action.sa_flags   = SA_NOCLDSTOP;
-
-    sigpairs[++i].signal          = SIGINT;
-    sigpairs[i].action.sa_handler = &terminate_children;
-
-    sigpairs[++i].signal          = SIGTERM;
-    sigpairs[i].action.sa_handler = &terminate_children;
-
-    /* setting sigcount now is easier than doing it dynamically */
-    sigcount = ++i;
-}
-
-/* Fork children, trap signals, block, and wait.
- *
- * Return values become exit values for the program.
- */
-int master()
-{
-    int i;
-
-    /* spawn some children */
-    for (i = 0; i < options.jobs; ++i) {
-        if (!child(i)) {
-            fprintf(stderr, "child() failed!\n");
-            return 1;
-        }
+        if (status = daemonize())
+            exit(status);
     }
 
-    register_signals();
-
-    /* Trap after forking children, during which SIGCHLD should be ignored
-     *
-     * This bit is new to me; forking many times in a row can lead to stillborn
-     * children. The program will handle this gracefully by itself if SIGCHLD
-     * is left alone during the spawning.
-     */
-    if (!trap_signals(true)) {
-        fprintf(stderr, "trap_signals() failed!\n");
-        return 1;
-    }
-
-    /* Block and wait for signals.
-     *
-     * An alternate, synchronous strategy to SIGCHLD could be deployed here,
-     * which is to call waitpid(..., WNOHANG) in a loop
-     */
-    while (1) sleep(1);
-
-    return 0;
+    return master();
 }
 
 /* Simple options parsing with getopt(), which has the old UNIX semantics.
@@ -374,49 +213,210 @@ int daemonize()
     return 0;
 }
 
-/* Entry routine; parse command line options and launch master process.
+/* Fork children, trap signals, block, and wait.
  *
- * REGARDING fork():
- *
- * Fork is a strange and unique system call. Along with exec(), it forms one of
- * the core features of Unix.
- *
- * At the moment fork() is called,
- *   - if the fork failed, it returns -1
- *   - if the fork succeeds, a duplicate of the parent process is formed,
- *     except that the caller is designated as the parent of new process
- *   - fork() returns the pid of the newly created process
- *   - BUT IN THE CHILD process, fork returns 0
- *
- * Because the child process will see fork() returning 0, and the parent will
- * see fork() returning a pid, we can carefully diverge the behavior of both to
- * do what we want!
+ * Return values become exit values for the program.
  */
-int main(int argc, char *argv[])
+int master()
 {
-    pid_t   pid;
-    int     status;
+    int i;
 
-    optparse(argc, argv);
-
-    if (options.daemonize) {
-        pid = fork();
-
-        if (pid < 0) {
-            /* fork failed, probably due to resource limits */
-            perror("fork()");
-            return errno;
-        } else if (pid > 0) {
-            /* fork successful! Tell us the forked processes's pid and exit */
-            printf("Forked master process: %d\n", pid);
-            return 0;
+    /* spawn some children */
+    for (i = 0; i < options.jobs; ++i) {
+        if (!child(i)) {
+            fprintf(stderr, "child() failed!\n");
+            return 1;
         }
-
-        /* Forked process continues here */
-
-        if (status = daemonize())
-            exit(status);
     }
 
-    return master();
+    register_signals();
+
+    /* Trap after forking children, during which SIGCHLD should be ignored
+     *
+     * This bit is new to me; forking many times in a row can lead to stillborn
+     * children. The program will handle this gracefully by itself if SIGCHLD
+     * is left alone during the spawning.
+     */
+    if (!trap_signals(true)) {
+        fprintf(stderr, "trap_signals() failed!\n");
+        return 1;
+    }
+
+    /* Block and wait for signals.
+     *
+     * An alternate, synchronous strategy to SIGCHLD could be deployed here,
+     * which is to call waitpid(..., WNOHANG) in a loop
+     */
+    while (1) sleep(1);
+
+    return 0;
+}
+
+/* Forks a child process, and returns true if successful.
+ * The forked child blocks and randomly dies.
+ *
+ * If this program did any work, the main communication between parent and child
+ * could take place through a pipe(2).
+ */
+bool child(int id)
+{
+    pid_t   pid;
+
+    /* see main() for discussion on fork() */
+    pid = fork();
+
+    if (pid < 0) {
+        return false;
+    } else if (pid > 0) {
+        printf("Spawning child %d (pid %d)\n", id, pid);
+        children[id] = pid; /* record the child pid */
+        return true;
+    }
+
+    /* Child process continues here */
+
+    /* Children processes are exact (almost) copies of the parent!
+     * Including signal traps! Our parent has a SIGTERM handler, and this child
+     * will dutifully execute the handler on SIGTERM unless we reset all
+     * signals back to their default handlers */
+    if (!trap_signals(false)) {
+        fprintf(stderr, "Child %d: trap_signals() failed!\n", id);
+        exit(1);
+    }
+
+    /* Block, and randomly die.
+     * If you're on Linux, arc4random() is why you need to link to libbsd
+     * (because it works, and I'm lazy) */
+    while (1) {
+        arc4random_stir();
+        if (arc4random() % 20)
+            sleep(1);
+        else
+            break;
+    }
+
+    exit(0);
+}
+
+/* Populate our array of custom handler objects */
+void register_signals()
+{
+    int i = 0;
+
+    sigpairs[i].signal            = SIGCHLD;
+    sigpairs[i].action.sa_handler = &restart_children;
+    /* Don't send SIGCHLD when a process has been frozen (e.g. Ctrl-Z) */
+    sigpairs[i].action.sa_flags   = SA_NOCLDSTOP;
+
+    sigpairs[++i].signal          = SIGINT;
+    sigpairs[i].action.sa_handler = &terminate_children;
+
+    sigpairs[++i].signal          = SIGTERM;
+    sigpairs[i].action.sa_handler = &terminate_children;
+
+    /* setting sigcount now is easier than doing it dynamically */
+    sigcount = ++i;
+}
+
+/* When passed true, traps signals and assigns handlers as defined in sigpairs[]
+ * When passed false, resets all trapped signals back to their default behavior.
+ */
+int trap_signals(bool on)
+{
+    int i;
+    struct sigaction dfl;       /* the handler object */
+
+    dfl.sa_handler = SIG_DFL;   /* for resetting to default behavior */
+
+    /* Loop through all registered signals and either set to the new handler
+     * or reset them back to the default */
+    for (i = 0; i < sigcount; ++i) {
+        /* notice that the second parameter takes the address of the handler */
+        if (sigaction(sigpairs[i].signal, on ? &sigpairs[i].action : &dfl, NULL) < 0)
+            return false;
+    }
+
+    return true;
+}
+
+/* Master's SIGCHLD handler.
+ *
+ * When a process is fork()ed by a process, the new process is an exact copy
+ * of the old process, except for a few values, one of which is that the parent
+ * pid of the child is that of the process that forked it.
+ *
+ * When this child exits, the signal SIGCHLD is sent to the parent process to
+ * alert it. By default, the signal is ignored, but we can take this opportunity
+ * to restart any children that have died.
+ *
+ * There are many ways to determine which children have died, but the most
+ * portable method is to use the wait() family of system calls.
+ *
+ * A dead child process releases its memory, but sticks around so that any
+ * interested parties can determine how they died (exit status). Calling wait()
+ * in the master collects the status of the first available dead process, and
+ * removes it from the process table.
+ *
+ * If wait() is never called by the parent, the dead child sticks around as a
+ * "zombie" process, marked with status `Z' in ps output. If the parent process
+ * exits without ever calling wait, the zombie process does not disappear, but
+ * is inherited by the root process (its parent pid is set to 1).
+ *
+ * Because SIGCHLD is an asynchronous signal, it is possible that if many
+ * children die simultaneously, the parent may only notice one SIGCHLD when many
+ * have been sent. In order to beat this edge case, we can simply loop through
+ * all the known children and call waitpid() in non-blocking mode to see if they
+ * have died, and spawn a new one in their place.
+ */
+void restart_children()
+{
+    int     i, status[0xff];    /* array of exit statuses, just in case */
+    pid_t   pid;
+
+    /* Racing signals may let dead children through the cracks,
+     * so check all children
+     */
+    for (i = 0; i < options.jobs; ++i) {
+        pid = waitpid(children[i], &status[i], WNOHANG); /* non-blocking! */
+
+        if (pid < 0) {
+            perror("waitpid()");
+        } else if (!pid) {
+            /* waitpid returns 0 when the process is alive and well */
+            continue;
+        } else {
+            printf("Master: reaped dead child %d (pid %d)\n", i, pid);
+            child(i); /* relaunch this particular child */
+        }
+    }
+}
+
+/* Master's kill switch
+ *
+ * It's important to ensure that all children have exited before the master
+ * exits so no root zombies are created. The default handler for SIGINT sends
+ * SIGINT to all children, but this is not true with SIGTERM.
+ */
+void terminate_children()
+{
+    int     i, status;
+    pid_t   pid;
+
+    printf("Termination signal received! Killing children");
+
+    /* Reset all signals! If the SIGCHLD handler in particular is not reset,
+     * all terminated children will be restarted
+     */
+    trap_signals(false);
+
+    for (i = 0; i < options.jobs; ++i)
+        kill(children[i], SIGTERM);
+
+    /* reap all children as they die, until none are left;
+     * at that point wait() returns -1 */
+    while ((pid = wait(&status)) != -1)
+        printf(".");
+
+    printf("\nAll children reaped, shutting down.\n");
+    exit(0);
 }
